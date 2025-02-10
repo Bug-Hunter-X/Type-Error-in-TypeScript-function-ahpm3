afbfe60a5e5733f7471322685f1d4f16a6f44f7a
BugHunter-X <askzenai@gmail.com>
2025-02-10 07:22:04 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,5 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+let result = add("hello", "world"); // This will compile without error but will not give the correct result at runtime
